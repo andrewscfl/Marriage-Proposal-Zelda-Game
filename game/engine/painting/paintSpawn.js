e2d6504c604a63b70f49e4f11675c62f0e494b23
andrew <andrewscaldwell@gmail.com>
2021-12-14 20:30:21 -0500

    wall elements built

--- game/engine/painting/paintSpawn.js
+++ game/engine/painting/paintSpawn.js
@@ -13,20 +13,47 @@ export default function (callback) {
 
     const wallElementOne = document.createElement('div')
     wallElementOne.classList.add('wall')
+    wallElementOne.setAttribute('style', `
+    width: ${window.innerWidth}px;
+    bottom: 0px;
+    height: 30px;
+    `);
+
+    const wallElementTwo = document.createElement('div')
+    wallElementTwo.classList.add('wall')
+    wallElementTwo.setAttribute('style', `
+    width: 30px;
+    height: ${window.innerHeight}px;
+    left: 0;
+    top: 0;
+    `)
+
+    const wallElementThree = document.createElement('div')
+    wallElementThree.classList.add('wall')
+    wallElementThree.setAttribute('style', `
+    width: 30px;
+    right: 0;
+    top: 0;
+    height: ${window.innerHeight}px
+    `)
 
 
     document.body.appendChild(enemyElement)
     document.body.appendChild(heroElement)
     document.body.appendChild(wallElementOne)
+    document.body.appendChild(wallElementTwo)
+    document.body.appendChild(wallElementThree)
+
+
 
     const heroObject = new hero(heroElement)
     const enemyObject = new enemy(enemyElement)
-    const wallObjectOne = new collider(wallElementOne)
+    new collider(wallElementOne)
+    new collider(wallElementTwo)
+    new collider(wallElementThree)
 
-    heroObject.setPositionPX(window.innerWidth / 2 - (heroElement.getBoundingClientRect().width / 2), window.innerHeight - (heroElement.getBoundingClientRect().height))
-    enemyObject.setPositionPX(window.innerWidth/2 - (enemyElement.getBoundingClientRect().width /2), window.innerHeight - (enemyElement.getBoundingClientRect().height) - 600 )
-    wallObjectOne.setPositionPX(window.innerWidth/2 - (enemyElement.getBoundingClientRect().width /2), window.innerHeight - (enemyElement.getBoundingClientRect().height) - 300 )
-
+    heroObject.setPositionPX(window.innerWidth / 2 - (heroElement.getBoundingClientRect().width / 2), window.innerHeight - (heroElement.getBoundingClientRect().height) - 100)
+    enemyObject.setPositionPX(window.innerWidth/2 - (enemyElement.getBoundingClientRect().width /2), window.innerHeight - (enemyElement.getBoundingClientRect().height) - 400 )
 
 
     const enemyAttack = setInterval(() => {
